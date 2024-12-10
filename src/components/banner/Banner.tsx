ddec2d47620761e97a031d2f9b34e2374e441d25
Exim3 <exim3@maatson.com>
2024-12-10 15:30:40 +0530

    dec-10

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -8,14 +8,16 @@ interface BannerProps {
 
 const Banner: React.FC<BannerProps> = ({ imageLink, title, content }) => {
   return (
-    <div className="py-[95px] relative container mx-auto bg-primary-100 flex flex-col gap-4 text-baseBlack text-center">
-      <div
-        className={`absolute inset-0  top-0 w-full left-0 opacity-20 bg-center`}
-        style={{ backgroundImage: `url(${imageLink})` }}
-      ></div>
-      <div className="min-h-10"></div>
-      <h1 className="text-5xl font-robotoSlab font-semibold ">{title}</h1>
-      <p>{content}</p>
+    <div className="relative bg-primary-100 ">
+      <div className="py-[95px]  container mx-auto  flex flex-col gap-4 text-baseBlack text-center">
+        <div
+          className={`absolute inset-0  top-0 w-full left-0 opacity-20 bg-center`}
+          style={{ backgroundImage: `url(${imageLink})` }}
+        ></div>
+        <div className="min-h-10"></div>
+        <h1 className="text-5xl font-robotoSlab font-semibold ">{title}</h1>
+        <p>{content}</p>
+      </div>
     </div>
   );
 };
