f4fc4b3b9b88997ab0539eec94921ca739947900
Exim3 <exim3@maatson.com>
2024-12-13 15:37:29 +0530

    icon bug solved

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -168,7 +168,7 @@ export const AdvantageIcon: React.FC<IconProps> = ({
   return (
     <svg
       width={size}
-      height={color}
+      height={size}
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
